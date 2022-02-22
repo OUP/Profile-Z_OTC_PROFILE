7cfe17b2d72cb9ac70f61e09afb0421c83eed810
shanmugam bhaskaran <shanmugam.bhaskaran@oup.com>
2022-02-22 07:55:04 +0000

    initial version

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function(AppComponent) {
+    return AppComponent.extend("oup.otc.profile.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
